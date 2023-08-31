e4dcfdaf85296f73e3de6fc1b9fe8d57ac1403cd
Pawel Tanas <pawel.tanas@gmail.com>
2023-08-31 20:05:54 +0000

    some changes

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+LeanIPGrid = FastAPI()
+
+@LeanIPGrid.get("/")
+async def root():
+    return {"message": "Hellow WorlD!"}
+
+
+@LeanIPGrid.get("/cidr/{cidr}")
+async def get_cidr(cidr):
+    return {"cidr": cidr}
+
+@LeanIPGrid.get("/network/{network}")
+async def get_specific_network(network):
+    return {"network": network}
+
+@LeanIPGrid.get("/networks")
+async def list_networks():
+    return {"message": "list of all network"}
